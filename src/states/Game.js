6a20ed16e911495d4bd6a2c6afb4a18ff09da0f8
Pedro Martinez <pmartinez1085@gmail.com>
2017-05-05 17:40:40 +0200

    sounds & enemy & coins

--- src/states/Game.js
+++ src/states/Game.js
@@ -8,36 +8,105 @@ export default class extends Phaser.State {
     this.game.load.spritesheet('player', 'assets/player.png',28,22)
     this.game.load.image('ground', 'assets/ground.png')
     this.game.load.image('wall', 'assets/wall.png')
+    this.game.load.image('enemy', 'assets/enemy.png')
+    this.game.load.image('coin', 'assets/coin.png')
+    this.game.load.audio('jump', 'assets/jump.wav', '/assets/jump.mp3')
+    this.game.load.audio('coins', 'assets/coin.wav', '/assets/coin.mp3')
+    this.game.load.audio('dead', 'assets/dead.wav', '/assets/dead.mp3')
 
   }
   create (){
     this.game.physics.startSystem(Phaser.Physics.ARCADE)
-    this.player = game.add.sprite(250,50, 'player')
+    this.player = game.add.sprite(250,101, 'player')
+    this.enemy = game.add.sprite(400,400/2-20, 'enemy')
+    this.coin = game.add.sprite(420,100, 'coin')
     this.ground = game.add.sprite(760/2-160, 400/2,'ground')
     this.wall1 = game.add.sprite(760/2-160, 400/2-80,'wall')
     this.wall2 = game.add.sprite(760/2+140, 400/2-80,'wall')
+
+    this.jumpSound = this.game.add.audio('jump')
+    this.deadSound = this.game.add.audio('dead')
+    this.coinSound = this.game.add.audio('coins')
 
     game.physics.arcade.enable(this.player)
     game.physics.arcade.enable(this.ground)
     game.physics.arcade.enable(this.wall1)
     game.physics.arcade.enable(this.wall2)
+    game.physics.arcade.enable(this.enemy)
+    game.physics.arcade.enable(this.coin)
 
     this.player.body.gravity.y= 600
-    // this.ground.body.gravity.y= -600
+
     this.ground.body.immovable = true
+    this.wall1.body.immovable = true
+    this.wall2.body.immovable = true
+    this.enemy.body.immovable = true
+    this.coin.body.immovable = true
 
 
     this.player.body.setSize(20,20,0,0);
 
     this.player.animations.add('idle', [3,4,5,4],5,true)
     this.player.animations.play('idle')
-
+    this.cursor = game.input.keyboard.createCursorKeys()
+    this.hasJumped = false
 
   }
 
   update(){
 
     this.game.physics.arcade.collide(this.player, this.ground)
+    this.game.physics.arcade.collide(this.player, this.wall1)
+    this.game.physics.arcade.collide(this.player, this.wall2)
+    this.game.physics.arcade.collide(this.player, this.enemy)
+    this.game.physics.arcade.collide(this.player, this.coin)
+
+    this.inputs()
+    if(this.player.body.touching.down){
+      this.hasJumped = false
+    }
+
+
+  }
+
+  inputs (){
+
+    if (this.cursor.left.isDown){
+      this.player.body.velocity.x = -50
+      this.player.frame = 2
+    }
+    else if (this.cursor.right.isDown){
+      this.player.body.velocity.x = 50
+      this.player.frame = 2
+    }
+    else {
+      this.player.body.velocity.x = 0
+    }
+
+    if (this.cursor.up.isDown){
+      this.jumpPlayer();
+    }
+    if (this.game.physics.arcade.collide(this.player, this.enemy) == true){
+      this.deadPlayer();
+    }
+    if (this.game.physics.arcade.collide(this.player, this.coin) == true) {
+      this.coinPlayer();
+    }
+  }
+
+  jumpPlayer () {
+    this.player.body.velocity.y = -220
+    this.jumpSound.play()
+    this.hasJumped = true
+  }
+
+  coinPlayer () {
+    this.coinSound.play()
+
+  }
+
+  deadPlayer(){
+    this.deadSound.play()
 
 
   }

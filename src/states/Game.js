8023ad6c8fc9568d333fb2577974aed8f930956b
Pedro Martinez <pmartinez1085@gmail.com>
2017-05-05 16:32:21 +0200

    ground immovable

--- src/states/Game.js
+++ src/states/Game.js
@@ -5,19 +5,36 @@ import Mushroom from '../sprites/Mushroom'
 export default class extends Phaser.State {
   init () {}
   preload () {
-    this.game.load.spritesheet('player', 'assets/player.png')
+    this.game.load.spritesheet('player', 'assets/player.png',28,22)
+    this.game.load.image('ground', 'assets/ground.png')
 
   }
   create (){
     this.game.physics.startSystem(Phaser.Physics.ARCADE)
     this.player = game.add.sprite(250,50, 'player')
-    game.physics.arcade.enable(this.player)
-    this.player.body.gravity.y= 600
+    this.ground = game.add.sprite(760/2-160, 400/2,'ground')
 
-    this.player.animations.add('idle', [3,4,5,4])
+    game.physics.arcade.enable(this.player)
+    game.physics.arcade.enable(this.ground)
+
+    this.player.body.gravity.y= 600
+    // this.ground.body.gravity.y= -600
+    this.ground.body.immovable = true
+
+
+    this.player.body.setSize(20,20,0,0);
+
+    this.player.animations.add('idle', [3,4,5,4],5,true)
+    this.player.animations.play('idle')
+
+
   }
 
+  update(){
 
+    this.game.physics.arcade.collide(this.player, this.ground)
+
+  }
 
   render () {
     if (__DEV__) {

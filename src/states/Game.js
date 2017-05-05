15a3712eb8aa73057d3c63ab70f74cb9a47a7213
Pedro Martinez <pmartinez1085@gmail.com>
2017-05-05 15:56:23 +0200

    sync & physics

--- src/states/Game.js
+++ src/states/Game.js
@@ -4,31 +4,41 @@ import Mushroom from '../sprites/Mushroom'
 
 export default class extends Phaser.State {
   init () {}
-  preload () {}
+  preload () {
+    this.game.load.spritesheet('player', 'assets/player.png')
 
-  create () {
-    const bannerText = 'Phaser + ES6 + Webpack'
-    let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText)
-    banner.font = 'Bangers'
-    banner.padding.set(10, 16)
-    banner.fontSize = 40
-    banner.fill = '#77BFA3'
-    banner.smoothed = false
-    banner.anchor.setTo(0.5)
-
-    this.mushroom = new Mushroom({
-      game: this,
-      x: this.world.centerX,
-      y: this.world.centerY,
-      asset: 'mushroom'
-    })
-
-    this.game.add.existing(this.mushroom)
   }
+  create (){
+    this.game.physics.startSystem(Phaser.Physics.ARCADE)
+    this.player = game.add.sprite(250,50, 'player')
+  }
+
+
 
   render () {
     if (__DEV__) {
-      this.game.debug.spriteInfo(this.mushroom, 32, 32)
+      //this.game.debug.spriteInfo(this.mushroom, 32, 32)
     }
   }
 }
+
+
+// create () {
+//   const bannerText = 'Phaser + ES6 + Webpack'
+//   let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText)
+//   banner.font = 'Bangers'
+//   banner.padding.set(10, 16)
+//   banner.fontSize = 40
+//   banner.fill = '#77BFA3'
+//   banner.smoothed = false
+//   banner.anchor.setTo(0.5)
+//
+//   this.mushroom = new Mushroom({
+//     game: this,
+//     x: this.world.centerX,
+//     y: this.world.centerY,
+//     asset: 'mushroom'
+//   })
+//
+//   this.game.add.existing(this.mushroom)
+// }
